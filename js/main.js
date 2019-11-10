c74c57a25a8ed17d898a18ee2e4b8ba53012efef
Paulwamaria <paulwamaria@gmail.com>
2019-11-10 15:52:46 +0300

    Add a function to controll navbar button

--- js/main.js
+++ js/main.js
@@ -24,6 +24,28 @@ const t1 = new TimelineMax();
 t1.fromTo(hero,2, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
 
 
+function closeOpen() {
+    var navb = document.getElementById("navbox");
+
+    if (navb.style.width == "300px") {
+        navb.style.width = "0px";
+     
+        navs.style.opacity = "0";
+        btn.style.top = "10px";
+        d1.classList.remove("d1");
+        d2.classList.remove("d2");
+        d3.classList.remove("d3");
+    } else {
+        navb.style.width = "300px";
+   
+        navs.style.opacity = "1";
+        btn.style.top = "18px";
+        d1.classList.add("d1");
+        d2.classList.add("d2");
+        d3.classList.add("d3");
+    }
+
+}
 
 
 
