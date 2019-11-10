1badde40eb8423a4408be92530ec8aee983930dd
Paulwamaria <paulwamaria@gmail.com>
2019-11-10 08:19:54 +0300

    Add a  fade in effect to headline

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+
+
+
+  $(document).ready(function() {
+
+        $.fx.speeds.xslow = 100000000;
+        $('.headline').fadeIn(5000);
+
+  });
+  
+
+
+
 const hero=document.querySelector('.hero');
 const slider=document.querySelector('.slider');
 const logo=document.querySelector('#logo');
@@ -8,4 +21,9 @@ const headline=document.querySelector('.headline');
 const t1 = new TimelineMax();
 
 
-t1.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
+t1.fromTo(hero,2, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
+
+
+
+
+
