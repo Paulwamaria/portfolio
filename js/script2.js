135b0d43e61fd0293ecf0eed741ed60c3f9d922f
Paulwamaria <paulwamaria@gmail.com>
2019-11-11 13:21:21 +0300

    Add another script for projects

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+
+// $(window).load(function() {
+ 
+
+//     $('.carousel').carousel('pause');
+ 
+// });
+  $(document).ready(function() {
+    // $("#myCarousel").carousel({
+    //     interval : false
+    // });
+    $('.carousel').carousel('pause');
+
+    // $("#emryo").click(function(){
+    //     $("#emryon").show();
+    // });
+  
+
+});
